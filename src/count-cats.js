3e9ffb967bf7f25280cf61f558272c9a2415a0d5
vasilevski-as <vasilevski.as@gmail.com>
2022-10-13 16:53:54 +0300

    feat: count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,10 +14,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+const matrix = [
+  ["^^", ".", null, 0, false, "", NaN, "^^", 2, true, "dasdas", 1],
+  [2, null, 0, 1, ".", "dasdas", true, NaN, "", false, "^^", "^^"],
+  [false, ".", 1, 0, "^^", null, "", 2, "dasdas", "^^", NaN, true],
+  [".", false, 1, null, NaN, 2, 0, "dasdas", true, "^^", "", "^^"],
+  [false, ".", 1, 0, "^^", true, null, "^^", "", NaN, 2, "dasdas"],
+  [false, NaN, 1, 0, ".", "^^", null, true, "dasdas", "^^", 2, ""],
+  [null, 1, NaN, true, ".", "^^", "^^", 2, "", false, "dasdas", 0],
+  [null, NaN, "", false, ".", 1, 0, "^^", "dasdas", true, 2, "^^"],
+];
+
+function countCats(matrix) {
+  let Number = 0;
+  matrix.forEach(element => {
+    Number += (element.filter(i => (i == '^^')).length);
+  });
+return Number;
 }
+countCats(matrix);
+
 
 module.exports = {
   countCats
